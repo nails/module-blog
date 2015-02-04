d617c69b682d35168b5a4b3820f54947558d1746
Pablo de la Pena <p@shedcollective.org>
2015-02-04 00:56:42 +0000

    New admin nav system

---
+++ admin/controllers/post.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Manage Blog posts
+ *
+ * @package     Nails
+ * @subpackage  module-blog
+ * @category    AdminController
+ * @author      Nails Dev Team
+ * @link
+ */
+
+namespace Nails\Admin\Blog;
+
+class Post extends \AdminController
+{
+   /**
+     * Announces this controller's navGroupings
+     * @return stdClass
+     */
+    public static function announce()
+    {
+        //  Fetch the blogs, each blog should have its own admin nav grouping
+        $ci =& get_instance();
+        $ci->load->model('blog/blog_model');
+        $blogs = $ci->blog_model->get_all();
+
+        $out = array();
+
+        if (!empty($blogs)) {
+
+            foreach ($blogs as $blog) {
+
+                //  Clear group naming
+                $groupLabel = count($blogs) > 1 ? 'Blog: ' . $blog->label : 'Blog';
+
+                //  Create the navGrouping
+                $navGroup = new \Nails\Admin\Nav($groupLabel);
+                $navGroup->addMethod('Manage Posts', 'index/' . $blog->id);
+
+                $out[] = $navGroup;
+            }
+        }
+
+        return $out;
+    }
+}
